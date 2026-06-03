20d00ef1adb7704820f5ce03a57ab6fd3302a1a8
Arshid143 <marshad143@yahoo.com>
2026-06-03 15:27:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ let index=0;
 
 const grid=document.getElementById("grid");
 
+// GRID
 Object.keys(data).forEach(letter=>{
 let div=document.createElement("div");
 div.className="card";
@@ -26,6 +27,7 @@ div.onclick=()=>openPopup(letter);
 grid.appendChild(div);
 });
 
+// POPUP
 function openPopup(letter){
 currentLetter=letter;
 index=0;
@@ -66,7 +68,71 @@ if(e.target.id==="popup") closePopup();
 }
 
 function speak(text){
+speechSynthesis.cancel();
 let msg=new SpeechSynthesisUtterance(text);
 msg.rate=0.9;
 speechSynthesis.speak(msg);
+}
+
+function repeatSpeak(){
+showItem();
+}
+
+// ================= QUIZ =================
+
+let quizLetter="";
+let quizIndex=0;
+
+function openQuiz(){
+document.getElementById("quiz").classList.remove("hidden");
+startQuiz();
+}
+
+function startQuiz(){
+let letters=Object.keys(data);
+quizLetter=letters[Math.floor(Math.random()*letters.length)];
+quizIndex=0;
+
+document.getElementById("question").innerText="Find: "+quizLetter;
+
+let options=document.getElementById("options");
+options.innerHTML="";
+
+let correct=quizLetter;
+
+let choices=["A","B","C","D","E","F","G","H"];
+let mix=[correct];
+
+while(mix.length<4){
+let r=choices[Math.floor(Math.random()*choices.length)];
+if(!mix.includes(r)) mix.push(r);
+}
+
+mix.sort(()=>Math.random()-0.5);
+
+mix.forEach(l=>{
+let btn=document.createElement("button");
+btn.innerText=l;
+btn.onclick=()=>checkAnswer(l);
+options.appendChild(btn);
+});
+}
+
+function checkAnswer(ans){
+if(ans===quizLetter){
+speak("Correct");
+alert("🎉 Correct!");
+startQuiz();
+}else{
+speak("Try again");
+alert("❌ Try Again");
+}
+}
+
+function closeQuiz(){
+document.getElementById("quiz").classList.add("hidden");
+}
+
+function closeQuizOutside(e){
+if(e.target.id==="quiz") closeQuiz();
 }
