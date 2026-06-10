c3d91185ed96b21006c6e70f7b52b2d25c79d65b
Arshid143 <marshad143@yahoo.com>
2026-06-10 09:38:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -17,167 +17,167 @@ const colors = [
 
 const data = {
 A:[
-{name:"Apple",img:"assets/alphabet/apple.webp"},
-{name:"Arm",img:"assets/alphabet/arm.webp"},
-{name:"Ant",img:"assets/alphabet/ant.webp"},
-{name:"Aeroplane",img:"assets/alphabet/aeroplane.webp"}
+{name:"Apple",img:"../../assets/apple.webp"},
+{name:"Arm",img:"../../assets/arm.webp"},
+{name:"Ant",img:"../../assets/ant.webp"},
+{name:"Aeroplane",img:"../../assets/aeroplane.webp"}
 ],
 B:[
-{name:"Ball",img:"assets/alphabet/ball.webp"},
-{name:"Banana",img:"assets/alphabet/banana.webp"},
-{name:"Bag",img:"assets/alphabet/bag.webp"},
-{name:"Book",img:"assets/alphabet/book.webp"},
-{name:"Bus",img:"assets/alphabet/bus.webp"}
+{name:"Ball",img:"../../assets/ball.webp"},
+{name:"Banana",img:"../../assets/banana.webp"},
+{name:"Bag",img:"../../assets/bag.webp"},
+{name:"Book",img:"../../assets/book.webp"},
+{name:"Bus",img:"../../assets/bus.webp"}
 ],
 C:[
-{name:"Cat",img:"assets/alphabet/cat.webp"},
-{name:"Car",img:"assets/alphabet/car.webp"},
-{name:"Cake",img:"assets/alphabet/cake.webp"},
-{name:"Cow",img:"assets/alphabet/cow.webp"},
-{name:"Cup",img:"assets/alphabet/cup.webp"}
+{name:"Cat",img:"../../assets/cat.webp"},
+{name:"Car",img:"../../assets/car.webp"},
+{name:"Cake",img:"../../assets/cake.webp"},
+{name:"Cow",img:"../../assets/cow.webp"},
+{name:"Cup",img:"../../assets/cup.webp"}
 ],
 D:[
-{name:"Dog",img:"assets/alphabet/dog.webp"},
-{name:"Duck",img:"assets/alphabet/duck.webp"},
-{name:"Drum",img:"assets/alphabet/drum.webp"},
-{name:"Door",img:"assets/alphabet/door.webp"}
+{name:"Dog",img:"../../assets/dog.webp"},
+{name:"Duck",img:"../../assets/duck.webp"},
+{name:"Drum",img:"../../assets/drum.webp"},
+{name:"Door",img:"../../assets/door.webp"}
 ],
 E:[
-{name:"Egg",img:"assets/alphabet/egg.webp"},
-{name:"Elephant",img:"assets/alphabet/elephant.webp"},
-{name:"Eye",img:"assets/alphabet/eye.webp"},
-{name:"Ear",img:"assets/alphabet/ear.webp"}
+{name:"Egg",img:"../../assets/egg.webp"},
+{name:"Elephant",img:"../../assets/elephant.webp"},
+{name:"Eye",img:"../../assets/eye.webp"},
+{name:"Ear",img:"../../assets/ear.webp"}
 ],
 F:[
-{name:"Fish",img:"assets/alphabet/fish.webp"},
-{name:"Fan",img:"assets/alphabet/fan.webp"},    
-{name:"Finger",img:"assets/alphabet/finger.webp"},
-{name:"Fox",img:"assets/alphabet/fox.webp"},
-{name:"Flag",img:"assets/alphabet/flag.webp"}
+{name:"Fish",img:"../../assets/fish.webp"},
+{name:"Fan",img:"../../assets/fan.webp"},    
+{name:"Finger",img:"../../assets/finger.webp"},
+{name:"Fox",img:"../../assets/fox.webp"},
+{name:"Flag",img:"../../assets/flag.webp"}
 ],
 G:[
-{name:"Goat",img:"assets/alphabet/goat.webp"},
-{name:"Girl",img:"assets/alphabet/girl.webp"},
-{name:"Grapes",img:"assets/alphabet/grapes.webp"},
-{name:"Gift",img:"assets/alphabet/gift.webp"},
-{name:"Gun",img:"assets/alphabet/gun.webp"}
+{name:"Goat",img:"../../assets/goat.webp"},
+{name:"Girl",img:"../../assets/girl.webp"},
+{name:"Grapes",img:"../../assets/grapes.webp"},
+{name:"Gift",img:"../../assets/gift.webp"},
+{name:"Gun",img:"../../assets/gun.webp"}
 ],
 H:[
-{name:"Hat",img:"assets/alphabet/hat.webp"},
-{name:"Horse",img:"assets/alphabet/horse.webp"},
-{name:"Hen",img:"assets/alphabet/hen.webp"},
-{name:"Hand",img:"assets/alphabet/hand.webp"},
-{name:"House",img:"assets/alphabet/house.webp"}
+{name:"Hat",img:"../../assets/hat.webp"},
+{name:"Horse",img:"../../assets/horse.webp"},
+{name:"Hen",img:"../../assets/hen.webp"},
+{name:"Hand",img:"../../assets/hand.webp"},
+{name:"House",img:"../../assets/house.webp"}
 ],
 I:[
-{name:"Ice cream",img:"assets/alphabet/icecream.webp"},
-{name:"Ink",img:"assets/alphabet/ink.webp"},
-{name:"Iron",img:"assets/alphabet/iron.webp"},
-{name:"Igloo",img:"assets/alphabet/igloo.webp"}
+{name:"Ice cream",img:"../../assets/icecream.webp"},
+{name:"Ink",img:"../../assets/ink.webp"},
+{name:"Iron",img:"../../assets/iron.webp"},
+{name:"Igloo",img:"../../assets/igloo.webp"}
 ],
 J:[
-{name:"Jug",img:"assets/alphabet/jug.webp"},
-{name:"Jam",img:"assets/alphabet/jam.webp"},
-{name:"Juice",img:"assets/alphabet/juice.webp"},
-{name:"Jar",img:"assets/alphabet/jar.webp"},
-{name:"Jeep",img:"assets/alphabet/jar.webp"}
+{name:"Jug",img:"../../assets/jug.webp"},
+{name:"Jam",img:"../../assets/jam.webp"},
+{name:"Juice",img:"../../assets/juice.webp"},
+{name:"Jar",img:"../../assets/jar.webp"},
+{name:"Jeep",img:"../../assets/jar.webp"}
 ],
 K:[
-{name:"Kite",img:"assets/alphabet/kite.webp"},
-{name:"King",img:"assets/alphabet/king.webp"},
-{name:"Key",img:"assets/alphabet/key.webp"},
-{name:"Knife",img:"assets/alphabet/knife.webp"}
+{name:"Kite",img:"../../assets/kite.webp"},
+{name:"King",img:"../../assets/king.webp"},
+{name:"Key",img:"../../assets/key.webp"},
+{name:"Knife",img:"../../assets/knife.webp"}
 ],
 L:[
-{name:"Loin",img:"assets/alphabet/loin.webp"},
-{name:"Lamp",img:"assets/alphabet/lamp.webp"},
-{name:"Leaf",img:"assets/alphabet/leaf.webp"},
-{name:"Lock",img:"assets/alphabet/lock.webp"}
+{name:"Loin",img:"../../assets/loin.webp"},
+{name:"Lamp",img:"../../assets/lamp.webp"},
+{name:"Leaf",img:"../../assets/leaf.webp"},
+{name:"Lock",img:"../../assets/lock.webp"}
 ],
 M:[
-{name:"Monkey",img:"assets/alphabet/monkey.webp"},
-{name:"Moon",img:"assets/alphabet/moon.webp"},,
-{name:"Mango",img:"assets/alphabet/mango.webp"},
-{name:"Milk",img:"assets/alphabet/milk.webp"},
-{name:"Mouse",img:"assets/alphabet/mouse.webp"}
+{name:"Monkey",img:"../../assets/monkey.webp"},
+{name:"Moon",img:"../../assets/moon.webp"},,
+{name:"Mango",img:"../../assets/mango.webp"},
+{name:"Milk",img:"../../assets/milk.webp"},
+{name:"Mouse",img:"../../assets/mouse.webp"}
 ],
 N:[
-{name:"Nest",img:"assets/alphabet/nest.webp"},
-{name:"Net",img:"assets/alphabet/net.webp"},
-{name:"Nose",img:"assets/alphabet/nose.webp"},
-{name:"Nail",img:"assets/alphabet/nail.webp"}
+{name:"Nest",img:"../../assets/nest.webp"},
+{name:"Net",img:"../../assets/net.webp"},
+{name:"Nose",img:"../../assets/nose.webp"},
+{name:"Nail",img:"../../assets/nail.webp"}
 ],
 O:[
-{name:"Orange",img:"assets/alphabet/orange.webp"},
-{name:"Owl",img:"assets/alphabet/owl.webp"},
-{name:"Ox",img:"assets/alphabet/ox.webp"},
-{name:"Oil",img:"assets/alphabet/oil.webp"}
+{name:"Orange",img:"../../assets/orange.webp"},
+{name:"Owl",img:"../../assets/owl.webp"},
+{name:"Ox",img:"../../assets/ox.webp"},
+{name:"Oil",img:"../../assets/oil.webp"}
 ],
 P:[
-{name:"Parrot",img:"assets/alphabet/parrot.webp"},
-{name:"Pen",img:"assets/alphabet/pen.webp"},
-{name:"Penguin",img:"assets/alphabet/penguin.webp"},
-{name:"Pencil",img:"assets/alphabet/pencil.webp"}
+{name:"Parrot",img:"../../assets/parrot.webp"},
+{name:"Pen",img:"../../assets/pen.webp"},
+{name:"Penguin",img:"../../assets/penguin.webp"},
+{name:"Pencil",img:"../../assets/pencil.webp"}
 ],
 Q:[
-{name:"Queen",img:"assets/alphabet/queen.webp"},
-{name:"Quail",img:"assets/alphabet/quail.webp"},
-{name:"Quilt",img:"assets/alphabet/quilt.webp"},
-{name:"Quack",img:"assets/alphabet/quack.webp"}
+{name:"Queen",img:"../../assets/queen.webp"},
+{name:"Quail",img:"../../assets/quail.webp"},
+{name:"Quilt",img:"../../assets/quilt.webp"},
+{name:"Quack",img:"../../assets/quack.webp"}
 ],
 R:[
-{name:"Rabbit",img:"assets/alphabet/rabbit.webp"},
-{name:"Rat",img:"assets/alphabet/rat.webp"},
-{name:"Ring",img:"assets/alphabet/ring.webp"},
-{name:"Rose",img:"assets/alphabet/rose.webp"}
+{name:"Rabbit",img:"../../assets/rabbit.webp"},
+{name:"Rat",img:"../../assets/rat.webp"},
+{name:"Ring",img:"../../assets/ring.webp"},
+{name:"Rose",img:"../../assets/rose.webp"}
 ],
 S:[
-{name:"Sun",img:"assets/alphabet/sun.webp"},
-{name:"School",img:"assets/alphabet/school.webp"},
-{name:"Star",img:"assets/alphabet/star.webp"},
-{name:"Snake",img:"assets/alphabet/snake.webp"}
+{name:"Sun",img:"../../assets/sun.webp"},
+{name:"School",img:"../../assets/school.webp"},
+{name:"Star",img:"../../assets/star.webp"},
+{name:"Snake",img:"../../assets/snake.webp"}
 ],
 T:[
-{name:"Tiger",img:"assets/alphabet/tiger.webp"},
-{name:"Train",img:"assets/alphabet/train.webp"},
-{name:"Truck",img:"assets/alphabet/truck.webp"},
-{name:"Table",img:"assets/alphabet/table.webp"}
+{name:"Tiger",img:"../../assets/tiger.webp"},
+{name:"Train",img:"../../assets/train.webp"},
+{name:"Truck",img:"../../assets/truck.webp"},
+{name:"Table",img:"../../assets/table.webp"}
 ],
 U:[
-{name:"Umbrella",img:"assets/alphabet/umbrella.webp"},
-{name:"Uniform",img:"assets/alphabet/uniform.webp"},
-{name:"Unicorn",img:"assets/alphabet/unicorn.webp"},
-{name:"Uncle",img:"assets/alphabet/uncle.webp"}
+{name:"Umbrella",img:"../../assets/umbrella.webp"},
+{name:"Uniform",img:"../../assets/uniform.webp"},
+{name:"Unicorn",img:"../../assets/unicorn.webp"},
+{name:"Uncle",img:"../../assets/uncle.webp"}
 ],
 V:[
-{name:"Van",img:"assets/alphabet/van.webp"},
-{name:"Vase",img:"assets/alphabet/vase.webp"},
-{name:"Violin",img:"assets/alphabet/violin.webp"},
-{name:"Village",img:"assets/alphabet/village.webp"}
+{name:"Van",img:"../../assets/van.webp"},
+{name:"Vase",img:"../../assets/vase.webp"},
+{name:"Violin",img:"../../assets/violin.webp"},
+{name:"Village",img:"../../assets/village.webp"}
 ],
 W:[
-{name:"Watch",img:"assets/alphabet/watch.webp"},
-{name:"Water",img:"assets/alphabet/water.webp"},
-{name:"Whale",img:"assets/alphabet/whale.webp"},
-{name:"Wolf",img:"assets/alphabet/wolf.webp"}
+{name:"Watch",img:"../../assets/watch.webp"},
+{name:"Water",img:"../../assets/water.webp"},
+{name:"Whale",img:"../../assets/whale.webp"},
+{name:"Wolf",img:"../../assets/wolf.webp"}
 ],
 X:[
-{name:"X-ray",img:"assets/alphabet/xray.webp"},
-{name:"Xylophone",img:"assets/alphabet/xylophone.webp"},
-{name:"X-box",img:"assets/alphabet/xbox.webp"},
-{name:"Xerus",img:"assets/alphabet/xerus.webp"}
+{name:"X-ray",img:"../../assets/xray.webp"},
+{name:"Xylophone",img:"../../assets/xylophone.webp"},
+{name:"X-box",img:"../../assets/xbox.webp"},
+{name:"Xerus",img:"../../assets/xerus.webp"}
 ],
 Y:[
-{name:"Yak",img:"assets/alphabet/yak.webp"},
-{name:"Yarn",img:"assets/alphabet/yarn.webp"},
-{name:"Yacht",img:"assets/alphabet/yacht.webp"},
-{name:"Yellow",img:"assets/alphabet/yellow.webp"}
+{name:"Yak",img:"../../assets/yak.webp"},
+{name:"Yarn",img:"../../assets/yarn.webp"},
+{name:"Yacht",img:"../../assets/yacht.webp"},
+{name:"Yellow",img:"../../assets/yellow.webp"}
 ],
 Z:[
-{name:"Zebra",img:"assets/alphabet/zebra.webp"},
-{name:"Zoo",img:"assets/alphabet/zoo.webp"},
-{name:"Zip",img:"assets/alphabet/zip.webp"},
-{name:"Zero",img:"assets/alphabet/zero.webp"}
+{name:"Zebra",img:"../../assets/zebra.webp"},
+{name:"Zoo",img:"../../assets/zoo.webp"},
+{name:"Zip",img:"../../assets/zip.webp"},
+{name:"Zero",img:"../../assets/zero.webp"}
 ]
 };
 
@@ -223,19 +223,25 @@ document.getElementById("popup-text").innerText=item.name;
 speak(currentLetter+" for "+item.name);
 }
 
-// NEXT
 function nextItem(){
-if(index<data[currentLetter].length-1){
-index++;
-showItem();
+let letters = Object.keys(data);
+let currentIndex = letters.indexOf(currentLetter);
+
+if(currentIndex < letters.length - 1){
+    currentLetter = letters[currentIndex + 1];
+    index = 0;
+    showItem();
 }
 }
 
-// PREV
 function prevItem(){
-if(index>0){
-index--;
-showItem();
+let letters = Object.keys(data);
+let currentIndex = letters.indexOf(currentLetter);
+
+if(currentIndex > 0){
+    currentLetter = letters[currentIndex - 1];
+    index = 0;
+    showItem();
 }
 }
 
@@ -247,6 +253,20 @@ document.getElementById("popup").classList.add("hidden");
 // OUTSIDE CLICK
 function outsideClick(e){
 if(e.target.id==="popup") closePopup();
+}
+
+function nextImage(){
+if(index < data[currentLetter].length - 1){
+    index++;
+    showItem();
+}
+}
+
+function prevImage(){
+if(index > 0){
+    index--;
+    showItem();
+}
 }
 
 // SPEAK
